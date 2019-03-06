8d4e16595812202fa11b9393a903a563c3489e1c
William J Teskey <willjteskey@gmail.com>
2019-03-06 21:18:30 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,27 +1,17 @@
-
 class PigLatinizer
 
-  def piglatinize(text)
-    text_array = text.split(" ").to_a
-    translated_array = []
-
-    text_array.each do |word|
-      alphabet = ('a'..'z').to_a + ('A'..'Z').to_a
-      vowels = ["a", "e", "i", "o", "u", "A", "I", "E", "O", "U"]
-      consonants = alphabet - vowels
-
-      if vowels.include?(word[0])
-        translated_array << (word + 'way')
-      elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
-        translated_array << (word[3..-1] + word[0..2] + 'ay')
-      elsif consonants.include?(word[0]) && consonants.include?(word[1])
-        translated_array << (word[2..-1] + word[0..1] + 'ay')
-      elsif consonants.include?(word[0])
-        translated_array << (word[1..-1] + word[0] + 'ay')
-      else
-        translated_array << word
-      end
-    end
-      translated_array.join(" ")
+  def piglatinize(word)
+    ( word[0].match(/[aeiou]/i) ) ? word + 'way' : consonant_word(word)
   end
+
+  def consonant_word(word)
+    first_vowel_index = word.index(/[aeiou]/) || 0
+
+    word[first_vowel_index, word.length] + word[0, first_vowel_index] + 'ay'
+  end
+
+  def to_pig_latin(sentence)
+    sentence.split(' ').map { |word|  piglatinize(word) }.join(' ')
+  end
+
 end
