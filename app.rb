8d8486d6ee90adc91c0e26b614f46cef9c9b3e4d
William J Teskey <willjteskey@gmail.com>
2019-03-06 21:16:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_index
+  end
+
+  post '/piglatinize' do
+    @translated_text = PigLatinizer.new.piglatinize(params[:user_phrase])
+    erb :results
+  end
+
 end
